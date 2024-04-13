b1c76d69eaf73ffedd236ab7ec986a8f06b8d2cc
BethFromAbove <bethcott@hotmail.co.uk>
2024-04-13 21:55:06 +0100

    Preliminary power bar

--- src/scenes/Game.js
+++ src/scenes/Game.js
@@ -1,5 +1,11 @@
 import { Scene } from 'phaser';
 
+var keys;
+var rectangle;
+var graphics;
+var coolometerCount;
+var coolometerMax;
+
 export class Game extends Scene
 {
     constructor ()
@@ -13,16 +19,53 @@ export class Game extends Scene
 
         this.add.image(512, 384, 'background').setAlpha(0.5);
 
-        this.add.text(512, 384, 'Make something fun!\nand share it with us:\nsupport@phaser.io', {
-            fontFamily: 'Arial Black', fontSize: 38, color: '#ffffff',
-            stroke: '#000000', strokeThickness: 8,
-            align: 'center'
-        }).setOrigin(0.5);
+        this.coolometerBackground = this.add.image(725, 300, 'powerbar-background');
+        this.coolometerForeground = this.add.image(725, 300, 'powerbar-foreground');
+        this.coolometerForeground.setDepth(3);
 
-        this.input.once('pointerdown', () => {
-
-            this.scene.start('GameOver');
-
-        });
+        this.addCoolometer();
     }
+
+    update () {
+        if (coolometerCount < coolometerMax){
+            coolometerCount = Math.min(coolometerMax, coolometerCount + 0.6);
+        }
+        else if (coolometerCount > 0){
+            coolometerCount = Math.max(0, coolometerCount - 3);
+        }
+
+         // update coolometer
+        graphics.clear();
+        rectangle.setSize(100, coolometerCount);
+        rectangle.y = 600 - coolometerCount;
+        graphics.fillRectShape(rectangle);
+    }
+
+    addCoolometer() {
+        graphics = this.add.graphics({ fillStyle: { color: 0x00ffff }});
+        rectangle = new Phaser.Geom.Rectangle(
+            this.coolometerForeground.getTopLeft().x,
+            this.coolometerForeground.getTopLeft().y,
+            this.coolometerForeground.width,
+            this.coolometerForeground.height
+            );
+        coolometerCount = 0;
+        coolometerMax = 600;
+
+        const shape = this.make.graphics();
+        shape.fillStyle(0xffffff);
+        shape.beginPath();
+        shape.fillRoundedRect(
+            rectangle.x + 2,
+            rectangle.y + 8,
+            rectangle.width - 4,
+            rectangle.height - 16,
+            45
+        );
+
+        const mask = shape.createGeometryMask();
+        graphics.setMask(mask);
+    }
+
+
 }
--- src/scenes/Preloader.js
+++ src/scenes/Preloader.js
@@ -33,6 +33,8 @@ export class Preloader extends Scene
         this.load.setPath('assets');
 
         this.load.image('logo', 'logo.png');
+        this.load.image('powerbar-background', 'barBackground.png');
+        this.load.image('powerbar-foreground', 'barForeground.png');
     }
 
     create ()

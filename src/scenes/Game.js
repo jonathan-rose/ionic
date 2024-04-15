d083abcdc057ca317b341e27ffe696bdf03580ed
BethFromAbove <bethcott@hotmail.co.uk>
2024-04-15 21:20:24 +0100

    Power bar and health bar added. Both slowly increase and sharply decrease with space and enter as placeholders for damage and cannon shots.

--- src/scenes/Game.js
+++ src/scenes/Game.js
@@ -1,12 +1,13 @@
 import { Scene } from 'phaser';
 
+var keys;
 var rectanglePowerbar;
 var graphicsPowerbar;
-var powerbarCount;
+var powerbarCurrent;
 var powerbarMax;
-var graphicshealthbar;
-var rectanglehealthbar;
-var healthbarCount;
+var graphicsHealthbar;
+var rectangleHealthbar;
+var healthbarCurrent;
 var healthbarMax;
 
 export class Game extends Scene
@@ -30,82 +31,80 @@ export class Game extends Scene
         this.healthbarForeground = this.add.image(850, 400, 'powerbar-foreground');
         this.healthbarForeground.setDepth(3);
 
+        keys = this.input.keyboard.addKeys({
+            'enter': Phaser.Input.Keyboard.KeyCodes.ENTER,
+            'space': Phaser.Input.Keyboard.KeyCodes.SPACE,
+        });
+
         // Add power bar
-        graphicsPowerbar = this.add.graphics({ fillStyle: { color: 0x00ffff }});
+        graphicsPowerbar = this.add.graphics({ fillStyle: { color: 0xdeb841 }});
         rectanglePowerbar = new Phaser.Geom.Rectangle(
             this.powerbarForeground.getTopLeft().x,
             this.powerbarForeground.getTopLeft().y,
             this.powerbarForeground.width,
             this.powerbarForeground.height
             );
-        powerbarCount = 0;
+        powerbarCurrent = 600;
         powerbarMax = 600;
 
-        const shape1 = this.make.graphics();
-        shape1.fillStyle(0xffffff);
-        shape1.beginPath();
-        shape1.fillRoundedRect(
-            rectanglePowerbar.x,
-            rectanglePowerbar.y,
-            rectanglePowerbar.width,
-            rectanglePowerbar.height,
-            this.powerbarForeground.width/2
-        );
-
-        const mask1 = shape1.createGeometryMask();
-        graphicsPowerbar.setMask(mask1);
+        this.makeShapeMask(rectanglePowerbar, graphicsPowerbar);
 
         // Add Health bar
-        graphicshealthbar = this.add.graphics({ fillStyle: { color: 0xff00ff }});
-        rectanglehealthbar = new Phaser.Geom.Rectangle(
+        graphicsHealthbar = this.add.graphics({ fillStyle: { color: 0xe54489 }});
+        rectangleHealthbar = new Phaser.Geom.Rectangle(
             this.healthbarForeground.getTopLeft().x,
             this.healthbarForeground.getTopLeft().y,
             this.healthbarForeground.width,
             this.healthbarForeground.height
             );
-        healthbarCount = 0;
+        healthbarCurrent = 600;
         healthbarMax = 600;
 
-        const shape2 = this.make.graphics();
-        shape2.fillStyle(0xffffff);
-        shape2.beginPath();
-        shape2.fillRoundedRect(
-            rectanglehealthbar.x,
-            rectanglehealthbar.y,
-            rectanglehealthbar.width,
-            rectanglehealthbar.height,
-            this.healthbarForeground.width/2
-        );
-
-        const mask2 = shape2.createGeometryMask();
-        graphicshealthbar.setMask(mask2);
+        this.makeShapeMask(rectangleHealthbar, graphicsHealthbar)
 
     }
 
     update () {
-        if (powerbarCount < powerbarMax){
-            powerbarCount = Math.min(powerbarMax, powerbarCount + 0.6);
+        if (powerbarCurrent < powerbarMax){
+            powerbarCurrent = Math.min(powerbarMax, powerbarCurrent + 5);
         }
-        else if (powerbarCount > 0){
-            powerbarCount = Math.max(0, powerbarCount - 3);
+        if (healthbarCurrent < healthbarMax){
+            healthbarCurrent = Math.min(healthbarMax, healthbarCurrent + 5);
         }
 
-        if (healthbarCount < healthbarMax){
-            healthbarCount = Math.min(healthbarMax, healthbarCount + 0.6);
+        if (Phaser.Input.Keyboard.JustDown(keys.space)) {
+            healthbarCurrent = Math.max(0, healthbarCurrent - 100);
         }
-        else if (healthbarCount > 0){
-            healthbarCount = Math.max(0, healthbarCount - 3);
+        if (Phaser.Input.Keyboard.JustDown(keys.enter)) {
+            powerbarCurrent = Math.max(0, powerbarCurrent - 100);
         }
+
 
          // update powerbar and healthbar
         graphicsPowerbar.clear();
-        rectanglePowerbar.setSize(this.powerbarForeground.width, powerbarCount);
-        rectanglePowerbar.y = this.powerbarForeground.getBottomLeft().y - powerbarCount;
+        rectanglePowerbar.setSize(this.powerbarForeground.width, powerbarCurrent);
+        rectanglePowerbar.y = this.powerbarForeground.getBottomLeft().y - powerbarCurrent;
         graphicsPowerbar.fillRectShape(rectanglePowerbar);
 
-        graphicshealthbar.clear();
-        rectanglehealthbar.setSize(this.healthbarForeground.width, healthbarCount);
-        rectanglehealthbar.y = this.healthbarForeground.getBottomLeft().y - healthbarCount;
-        graphicshealthbar.fillRectShape(rectanglehealthbar);
+        graphicsHealthbar.clear();
+        rectangleHealthbar.setSize(this.healthbarForeground.width, healthbarCurrent);
+        rectangleHealthbar.y = this.healthbarForeground.getBottomLeft().y - healthbarCurrent;
+        graphicsHealthbar.fillRectShape(rectangleHealthbar);
+    }
+
+    makeShapeMask(rectangleBarType, graphicsType)
+    {
+        const shape = this.make.graphics();
+        shape.fillStyle(0xffffff);
+        shape.beginPath();
+        shape.fillRoundedRect(
+            rectangleBarType.x,
+            rectangleBarType.y,
+            rectangleBarType.width,
+            rectangleBarType.height,
+            this.powerbarForeground.width/2
+        );
+        const mask = shape.createGeometryMask();
+        graphicsType.setMask(mask);
     }
 }

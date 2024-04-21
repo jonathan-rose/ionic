6df87a583156390fb1574b5be9c9e10887432f20
Dave Kimber <davekimber@riverford.co.uk>
2024-04-21 14:18:00 +0100

    Make main menu fancier
    Adding an IONIC logo, and some slick animations to make the scene
    loading a bit juicier.
    
    Tried to keep everything inside the LogoEffects class, but in there it
    is an absolute garbage fire of tweens and graphics masking, not the
    easiest to modify, apologies.
    
    got the tweens for some nice swiping lines, want to have them ween
    into existence first, maybe some material design-like circle pop to
    start. need to mask off the logo too and reveal with last (or near
    last) line.
    
    Also want ot have a nice ion aura field effect happen after. Needs
    thinking about,

--- src/scenes/Preloader.js
+++ src/scenes/Preloader.js
@@ -9,9 +9,6 @@ export class Preloader extends Scene
 
     init ()
     {
-        //  We loaded this image in our Boot Scene, so we can display it here
-        this.add.image(512, 384, 'background');
-
         //  A simple progress bar. This is the outline of the bar.
         this.add.rectangle(512, 384, 468, 32).setStrokeStyle(1, 0xffffff);
 
@@ -50,6 +47,6 @@ export class Preloader extends Scene
         //  For example, you can define global animations here, so we can use them in other scenes.
 
         //  Move to the MainMenu. You could also swap this for a Scene Transition, such as a camera fade.
-        this.scene.start('Game');
+        this.scene.start('MainMenu');
     }
 }
